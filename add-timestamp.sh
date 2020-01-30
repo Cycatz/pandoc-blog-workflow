ff578cea5d486d7387caec5e04213d548bfc0c81
Cycatz <thesummernightcat@gmail.com>
2020-01-30 13:08:50 +0800

    add woff2 type

--- add-timestamp.sh
+++ add-timestamp.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-suff=ttf
-fonts=$(find ./build -name \*.${suff})
+fonts_ttf=$(find ./build -name \*.ttf)
+fonts_woff2=$(find ./build -name \*.woff2)
 timestamp=$(date +%s)
 
-for font in ${fonts};
+for font in ${fonts_ttf};
 do
-	mv ${font} ${font%.*}_${timestamp}.${suff}
+	mv ${font} ${font%.*}_${timestamp}.ttf
 done
+for font in ${fonts_woff2};
+do
+	mv ${font} ${font%.*}_${timestamp}.woff2
+done
+
 sed -i "s/.ttf/_${timestamp}&/" ./build/css/font.css
+sed -i "s/.woff2/_${timestamp}&/" ./build/css/font.css
 
 
